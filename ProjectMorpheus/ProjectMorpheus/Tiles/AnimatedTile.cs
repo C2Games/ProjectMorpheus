b80e990fa239be8bfdb9e238fc2068054f33054e
Corey Cormier <coreycormier@utexas.edu>
2016-06-15 22:19:43 -0500

    Tile classes

---
+++ ProjectMorpheus/ProjectMorpheus/Tiles/AnimatedTile.cs
@@ -0,0 +1,25 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMorpheus.Tiles
+{
+    internal abstract class AnimatedTile : Tile
+    {
+        private int frame = 0;
+
+        private abstract int animationLength();
+
+        public void Draw(Vector2f tilePos, RenderWindow window) {
+            base.Draw(tilePos, window);
+            frame++;
+            IntRect rect = tileSprite().TextureRect;
+            rect.Left = frame * tileSize;
+            tileSprite().TextureRect = rect;
+        }
+    }
+}
---
+++ ProjectMorpheus/ProjectMorpheus/Tiles/Tile.cs
@@ -0,0 +1,32 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMorpheus.Tiles
+{
+    internal class Tile
+    {
+        protected const int tileSize = 32;
+
+        protected abstract Sprite tileSprite();
+
+        public abstract int GetDefenseValue();
+
+        public abstract int GetMoveCost();
+
+        public abstract bool IsInfantryPassable();
+
+        public abstract bool IsVehiclePassable();
+
+        public abstract bool IsAirPassable();
+
+        public void Draw(Vector2f tilePos, RenderWindow window) {
+            tileSprite().Position = tilePos;
+            window.Draw(tileSprite());
+        }
+    }
+}
