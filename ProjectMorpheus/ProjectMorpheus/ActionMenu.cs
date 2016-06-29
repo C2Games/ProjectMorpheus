0354cc36ca236c429c55f55d54580ac998205c15
C2Games <chriscormier94@gmail.com>
2016-06-29 18:12:57 -0500

    actionmenu change

--- ProjectMorpheus/ProjectMorpheus/ActionMenu.cs
+++ ProjectMorpheus/ProjectMorpheus/ActionMenu.cs
@@ -15,8 +15,8 @@ namespace ProjectMorpheus
         private UnitAction[] actions;
         private Vector2f location;
         private int selected;
-        private float rectWidth = 150; //TODO: experiment for right values
-        private float textHeight = 35;
+        private float rectWidth = 100; //TODO: experiment for right values
+        private float textHeight = 17;
         private float rectHeight;
 
         public ActionMenu(UnitAction[] actions, int selected, Vector2f location) {
@@ -57,13 +57,14 @@ namespace ProjectMorpheus
             Vector2f mouseLocation = new Vector2f(InputManager.GetMouseLocation().X, InputManager.GetMouseLocation().Y);
             float mX = mouseLocation.X, menuX = location.X, mY = mouseLocation.Y, menuY = location.Y;
             if ((mX < (menuX + rectWidth)) && (mX > menuX) && (mY < (menuY + rectHeight)) && (mY > menuY)) {
-                selected = (int)((mY - 5) / textHeight) - 1;
+                selected = (int)(((mY-menuY) - 5) / textHeight);
+                if (selected > (actions.Length - 1)) selected = actions.Length - 1;
             }
         }
 
         public void Draw(RenderWindow window) {
             RectangleShape menuShape = new RectangleShape(new Vector2f(rectWidth, rectHeight)); //TODO: make fancier
-            CircleShape pointer = new CircleShape(10, 3);
+            CircleShape pointer = new CircleShape((int)textHeight/3, 3);
 
             menuShape.OutlineThickness = 3.0f;
             menuShape.FillColor = new Color(110, 110, 110); //TODO: don't hardcode? can change color based on map or something
@@ -74,16 +75,17 @@ namespace ProjectMorpheus
             pointer.Rotation = -30.0f;
             pointer.OutlineThickness = 1.0f;
             pointer.OutlineColor = new Color(0, 0, 0);
-            pointer.Position = menuShape.Position + new Vector2f(0, 5 + (textHeight / 2.0f) + textHeight * selected);
+            pointer.Position = menuShape.Position + new Vector2f(5, 3 + (textHeight / 2.0f) + textHeight * selected);
             pointer.FillColor = new Color(255, 0, 0);
             window.Draw(menuShape);
             window.Draw(pointer);
             Font actionStringFont = ContentManager.GetFont("testfont");
             //actionStringFont.
             for (int i = 0; i < actions.Length; ++i) {
-                Text actionString = new Text(actions[i].ToString(), actionStringFont); //TODO: change to context menu font
+                Text actionString = new Text(actions[i].ToString(), actionStringFont); //TODO: change to smaller context menu font
                 actionString.Color = new Color(255, 255, 255);
-                actionString.Position = menuShape.Position + new Vector2f(30, 5 + textHeight * i);
+                actionString.Scale = new Vector2f(0.5f,0.5f);
+                actionString.Position = menuShape.Position + new Vector2f(25, 5 + textHeight * i);
                 window.Draw(actionString);
             }
         }
